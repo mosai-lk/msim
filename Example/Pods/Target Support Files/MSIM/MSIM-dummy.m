79b4ceb35d873a61d87fb8ad8f07d0bc35686c27
leikai <leikai@mosainet.com>
2016-12-21 12:26:15 +0800

    commit

---
+++ Example/Pods/Target Support Files/MSIM/MSIM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MSIM : NSObject
+@end
+@implementation PodsDummy_MSIM
+@end
---
+++ Example/Pods/Target Support Files/Pods-MSIM_Tests/Pods-MSIM_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MSIM_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MSIM_Tests
+@end
